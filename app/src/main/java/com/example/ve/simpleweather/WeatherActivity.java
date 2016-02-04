ea91e0f5a0a85131f4729749de7cce9ec04ed56d
Vadim Eremichev <ve@anahoret.com>
2016-02-04 18:23:44 +0200

    full screen ability

--- app/src/main/java/com/example/ve/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/example/ve/simpleweather/WeatherActivity.java
@@ -7,12 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
